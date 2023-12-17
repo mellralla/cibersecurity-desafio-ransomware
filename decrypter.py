daafa31915f78add1b809d1e8e8f822a609e2bcd
melissaralla <melissaralla@brq.com>
2023-12-17 17:45:10 -0300

    ransomware

--- decrypter.py
+++ decrypter.py
@@ -15,7 +15,7 @@ decrypt_data = aes.decrypt(file_data)
 ## remover o arquivo criptografado
 os.remove(file_name)
 
-## criar o arquivo descriptografado
+## criar o arquivo descriptografado na pasta
 new_file = "teste.txt"
 new_file = open(f'{new_file}', "wb")
 new_file.write(decrypt_data)
